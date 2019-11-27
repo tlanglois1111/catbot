c562175323f681f1bd6f037061893a8abd0111a4
tlanglois1111 <tmlanglois@gmail.com>
2019-11-27 14:46:51 -0600

    IMU stuff

--- src/gyro.py
+++ src/gyro.py
@@ -11,7 +11,7 @@ import os
 import logging
 import logging.config
 
-from cat_bot_trt import Gyro
+import RTIMU
 
 logging_config = {
     'version': 1,
@@ -55,14 +55,68 @@ logging.config.dictConfig(logging_config)
 logger = logging.getLogger(__name__)
 
 
+class Gyro(threading.Thread):
+    """RTIMU encapsulates use of gyro device"""
+
+    def _load_rtimu_lib(self):
+
+        logger.info("Using settings file %s", self.SETTINGS_FILE + ".ini")
+        if not os.path.exists(self.SETTINGS_FILE + ".ini"):
+            logger.error("Settings file does not exist, will be created")
+
+        s = RTIMU.Settings(self.SETTINGS_FILE)
+        self.imu = RTIMU.RTIMU(s)
+
+        logger.info("IMU Name: %s", self.imu.IMUName())
+
+        if not self.imu.IMUInit():
+            logger.error("IMU Init Failed")
+            self.good = False
+        else:
+            logger.info("IMU Init Succeeded")
+            self.good = True
+            self.imu.setSlerpPower(0.02)
+            self.imu.setGyroEnable(True)
+            self.imu.setAccelEnable(True)
+            self.imu.setCompassEnable(True)
+
+            self.poll_interval = self.imu.IMUGetPollInterval()
+            logger.info("Recommended Poll Interval: %f", self.poll_interval)
+
+    def __init__(self, settings_path="../dataset/RTIMULib"):
+        threading.Thread.__init__(self)
+        self.SETTINGS_FILE = settings_path
+        self._load_rtimu_lib()
+        self.keep_running = True
+        self.data = []
+
+    def run(self):
+        logger.info("running gyro")
+        while self.keep_running:
+            if self.good and self.imu.IMURead():
+                self.data = self.imu.getIMUData()
+            time.sleep(self.poll_interval*1.0/1000.0)
+
+    def get_headings(self):
+        logger.info("about to get gyro reading")
+        if len(self.data) > 0:
+            return self.data["accel"]
+        else:
+            return self.data
+
+    def stop(self):
+        self.keep_running = False
+
+
+
 def main():
 
-    gyro = Gyro()
-    gyro.start()
+    accel = Gyro()
+    accel.start()
 
     while True:
-        logger.info(gyro.get_headings())
-        time.sleep(2000)
+        logger.info(accel.get_headings())
+        time.sleep(500)
 
 
 if __name__ == '__main__':
