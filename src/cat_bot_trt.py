3286d942ca151853ba5e9b419d33087e77886415
tlanglois1111 <tmlanglois@gmail.com>
2019-11-30 10:30:23 -0600

    reverse and try try again

--- src/cat_bot_trt.py
+++ src/cat_bot_trt.py
@@ -87,7 +87,7 @@ SUPPORTED_MODELS = [
 ]
 IMAGE_DIR = '../dataset/cats'
 FORWARD_SPEED = 0.8
-BACKWARD_SPEED = 0.6
+BACKWARD_SPEED = -0.6
 TURNING_SPEED = 0.6
 
 def parse_args():
@@ -335,9 +335,8 @@ def loop_and_detect(cam, trt_ssd, conf_th, robot, model):
                         save_image(bgr8_to_jpeg(img), filename, blocked=True)
                         logger.info("blocked:  x: %.2f y: %.2f z: %.2f" % (accel[0], accel[1], accel[2]))
                         moving = False
-                        robot.backward(BACKWARD_SPEED)
+                        robot.set_motors(BACKWARD_SPEED, BACKWARD_SPEED/2)
                         time.sleep(0.5)
-                        robot.left(TURNING_SPEED)
                 counter = 0
 
             # compute all detected objects
