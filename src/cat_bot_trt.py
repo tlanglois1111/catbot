3a985dd5cd688c9f9077e4521da305f777eaee15
tlanglois1111 <tmlanglois@gmail.com>
2019-11-30 13:32:22 -0600

    calculate velocity

--- src/cat_bot_trt.py
+++ src/cat_bot_trt.py
@@ -266,19 +266,18 @@ def closest_detection(detections, width, height):
 
 moving = False
 acceleration = [[0, 0, 0], [0, 0, 0]]
-velocity = [[0, 0, 0], [0, 0, 0]]
+velocity = [0, 0, 0]
 
 
 def get_velocity(gyro):
-    for j in range(0,3):
+    for j in range(0, 3):
         acceleration[0] = acceleration[1]
-        velocity[0] = velocity[1]
         acceleration[1] = gyro["accel"]
 
-        velocity[1][j] = velocity[0][j] + acceleration[0][j] + ((acceleration[1][j] - acceleration[0][j]) / 2)
+        velocity[j] = acceleration[0][j] + ((acceleration[1][j] - acceleration[0][j]) / 2)
         #position[j][1] = position[j][0] + velocity[j][0] + ((velocity[j][1] - velocity[j][0]) / 2)
 
-        return velocity[1]
+        return velocity
 
 
 def loop_and_detect(cam, trt_ssd, conf_th, robot, model):
