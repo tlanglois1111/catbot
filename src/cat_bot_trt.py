18a89d3f1288dad652655ea3658873a0e5644bfc
tlanglois1111 <tmlanglois@gmail.com>
2019-12-07 13:46:45 -0600

    velocity not accurate so use compass to detect if turning/moving

--- src/cat_bot_trt.py
+++ src/cat_bot_trt.py
@@ -334,7 +334,7 @@ def loop_and_detect(cam, trt_ssd, conf_th, robot, model):
 
                 diff = np.absolute(old_compass - res)
                 logger.info("   diff:  x: %.4f y: %.4f z: %.4f" % (diff[0], diff[1], diff[2]))
-                if diff[2] > 0.03:
+                if diff[2] > 0.05:
                     moving = True
                 else:
                     moving = False
